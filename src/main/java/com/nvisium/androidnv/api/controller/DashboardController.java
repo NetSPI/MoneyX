36380ca5344dce2c1976c349c99351622a4d1d94
Seth Law <e3c92bc8878c2593a7f3b741f59ce74af19521a9>
2015-09-03 22:02:31 -0600

    Dashboard updates

--- src/main/java/com/nvisium/androidnv/api/controller/DashboardController.java
+++ src/main/java/com/nvisium/androidnv/api/controller/DashboardController.java
@@ -12,8 +12,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.nvisium.androidnv.api.model.Event;
-import com.nvisium.androidnv.api.model.User;
+import com.nvisium.androidnv.api.model.EventMembership;
 import com.nvisium.androidnv.api.service.EventService;
+import com.nvisium.androidnv.api.model.User;
+import com.nvisium.androidnv.api.service.UserService;
+import com.nvisium.androidnv.api.model.Payment;
+import com.nvisium.androidnv.api.service.PaymentService;
+import com.nvisium.androidnv.api.security.SecurityUtils;
 
 @RequestMapping()
 @Controller
@@ -22,6 +27,15 @@ public class DashboardController {
 	@Autowired
 	EventService eventService;
 	
+	@Autowired
+	SecurityUtils security;
+	
+	@Autowired
+	UserService userService;
+	
+	@Autowired
+	PaymentService paymentService;
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String listReceivedPayments() {
 		return "index";
@@ -29,13 +43,27 @@ public class DashboardController {
 	
 	@RequestMapping(value = "/dashboard", method = RequestMethod.GET)
 	public String dashboard(Model model) {
-		List<Event> events = eventService.getPublicEvents();
+		List<Event> owned = eventService.getEventsByOwner(security.getCurrentUserId());
 		Map<Event, List<User>> users = new HashMap<Event, List<User>>();
-		for (Event e: events) {
+		for (Event e: owned) {
 			users.put(e, eventService.getUsersbyEventMembership(e.getId()));
 		}
-		model.addAttribute("events", eventService.getPublicEvents());
+		List<EventMembership> memberships = eventService.getEventsByMembership(security.getCurrentUserId());
+		Map<EventMembership,Event> events = new HashMap<EventMembership,Event>();
+		for (EventMembership em: memberships) {
+			events.put(em,eventService.getEventById(em.getEventId()));
+			List<User> u = new java.util.ArrayList<User>();
+			u.add(userService.loadUserById(eventService.getEventById(em.getEventId()).getOwner()));
+			users.put(eventService.getEventById(em.getEventId()),u);
+		}
+		List<Payment> sent = paymentService.getSentPayments(userService.loadUserById(security.getCurrentUserId()));
+		List<Payment> received = paymentService.getReceivedPayments(userService.loadUserById(security.getCurrentUserId()));
+		
+		model.addAttribute("events", events);
+		model.addAttribute("owned", owned);
 		model.addAttribute("users", users);
+		model.addAttribute("sent", sent);
+		model.addAttribute("received", received);
 		return "dashboard";
 	}
 }
